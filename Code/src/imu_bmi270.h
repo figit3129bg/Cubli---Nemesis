391b46ae161da68464f17215736032d46daa894c
balaceanus <balaceanus@icloud.com>
2026-08-10 15:19:12 +0300

    noice

--- Code/src/imu_bmi270.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-extern float theta_b;
-extern float theta_b_dot;
-void imuSetup();
-void imuLoop();
--- Code/src/moteus_interface.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <limits>
-extern double theta_w_dot;
-void moteusSetup();
-void moteusSendTorque(double torque);
